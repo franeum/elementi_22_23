368ee8a8f52999cb2c0f6e770ffcf77fdbf995b9
neum <elettrochuck@gmail.com>
2023-01-19 17:48:25 +0100

    update

--- Lezione_05/indovina.py
+++ Lezione_05/indovina.py
@@ -2,30 +2,32 @@
 
 import os
 
-max = 1000
-min = 0
-current = max // 2
+if __name__ == "__main__":
+    max = 1000
+    min = 0
+    current_attempt = max // 2
 
-os.system("clear")
-input(f"PENSA UN NUMERO compreso fra 0 e {max} E PREMI INVIO")
+    os.system("clear")
+    input(f"PENSA UN NUMERO compreso fra 0 e {max} E PREMI UN TASTO")
 
-counter = 0
+    counter = 0
 
-while True:
-    attempt = input(f"E' IL {current}? ").strip(None)
+    while True:
+        attempt = input(f"È IL {current_attempt}? ").strip(None)
 
-    if attempt == "troppo basso":
-        min = current
-        counter += 1
-    elif attempt == "troppo alto":
-        max = current
-        counter += 1
-    elif attempt == "si":
-        print("MA QUANTO SO' BBRAVO!!!")
-        counter += 1
-        print(f"HO INDOVINATO IN {counter} TENTATIVI")
-        break
-    else:
-        print("non ti capisco; scrivi 'troppo basso' o 'troppo alto', oppure 'si' se la risposta è giusta")
+        if attempt == "troppo basso":
+            min = current_attempt
+            counter += 1
+        elif attempt == "troppo alto":
+            max = current_attempt
+            counter += 1
+        elif attempt == "si":
+            print("MA QUANTO SO' BBRAVO!!!")
+            counter += 1
+            print(f"HO INDOVINATO IN {counter} TENTATIVI")
+            break
+        else:
+            print(
+                "non ti capisco; scrivi 'troppo basso' o 'troppo alto', oppure 'si' se la risposta è giusta")
 
-    current = (min + max) // 2
+        current_attempt = (min + max) // 2
